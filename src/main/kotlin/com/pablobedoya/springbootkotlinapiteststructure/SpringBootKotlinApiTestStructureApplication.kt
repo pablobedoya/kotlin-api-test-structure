54dfea1d1bff80914905e5a3fd4d3e45d2d742a4
Pablo Bedoya <pablo.reds@hotmail.com>
2020-10-20 00:02:01 -0300

    Adding basic project structure

---
+++ src/main/kotlin/com/pablobedoya/springbootkotlinapiteststructure/SpringBootKotlinApiTestStructureApplication.kt
@@ -0,0 +1,11 @@
+package com.pablobedoya.springbootkotlinapiteststructure
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootKotlinApiTestStructureApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootKotlinApiTestStructureApplication>(*args)
+}
---
+++ src/test/kotlin/com/pablobedoya/springbootkotlinapiteststructure/SpringBootKotlinApiTestStructureApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pablobedoya.springbootkotlinapiteststructure
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootKotlinApiTestStructureApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
