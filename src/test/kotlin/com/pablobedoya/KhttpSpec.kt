6bb6a95ede749a89fdc7b0d014b2d7c98a34c831
Pablo Bedoya <pablo.reds@hotmail.com>
2020-10-23 21:36:32 -0300

    Adding user list test

--- src/test/kotlin/com/pablobedoya/KhttpSpec.kt
+++ src/test/kotlin/com/pablobedoya/KhttpSpec.kt
@@ -7,6 +7,7 @@ import org.spekframework.spek2.style.gherkin.Feature
 
 object KhttpSpec : Spek({
     val baseUri = "https://reqres.in"
+
     Feature("Create user") {
         val path = "/api/users"
         var requestName: String? = null
@@ -20,13 +21,33 @@ object KhttpSpec : Spek({
             When("I submit a request to register it") {
                 response = khttp.post(url = baseUri + path, json = mapOf("name" to requestName, "job" to requestJob))
             }
-            Then("It should return that user was successfully created") {
+            Then("Should return that user was successfully created") {
                 val statusCode = response?.statusCode
                 val responseName = response?.jsonObject?.get("name")
                 val responseJob = response?.jsonObject?.get("job")
                 assertEquals(201, statusCode)
                 assertEquals(requestName, responseName)
                 assertEquals(requestJob, responseJob)
+            }
+        }
+    }
+
+    Feature("List users") {
+        val path = "/api/users"
+        var requestPage: Int? = null
+        var response: Response? = null
+        Scenario("Get users successfully") {
+            Given("I want to search users first page") {
+                requestPage = 1
+            }
+            When("I submit a request to get users first page") {
+                response = khttp.get(url = baseUri + path, params = mapOf("page" to requestPage.toString()))
+            }
+            Then("Should return users first page successfully") {
+                val statusCode = response?.statusCode
+                val responsePage = response?.jsonObject?.get("page")
+                assertEquals(200, statusCode)
+                assertEquals(requestPage, responsePage)
             }
         }
     }
